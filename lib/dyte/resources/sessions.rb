94a9c3fd8424c00b000509d543d56d710f01ae4b
Nick Warwick <nicholaswarwick@icloud.com>
2023-08-06 10:05:27 -0400

    Remove unused ession endpoints

--- lib/dyte/resources/sessions.rb
+++ lib/dyte/resources/sessions.rb
@@ -19,14 +19,6 @@ module Dyte
       Session.new get_request("sessions/#{session_id}/participants/#{participant_id}").body.dig("data")
     end
 
-    def update(meeting_id:, **attributes)
-      Session.new put_request("sessions/#{meeting_id}", body: attributes).body.dig("data", "session")
-    end
-
-    def update_old(meeting_id:, body: {})
-      Session.new put_request("sessions/#{meeting_id}", body: body).body.dig("data", "session")
-    end
-
     def chat_messages(session_id:)
       Session.new get_request("sessions/#{session_id}/chat").body.dig("data")
     end
