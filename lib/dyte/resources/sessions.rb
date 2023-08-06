1155af1b7c73ae2be10c61fdad201af900440363
Nick Warwick <nicholaswarwick@icloud.com>
2023-08-06 09:31:02 -0400

    Add missing session endpoints

--- lib/dyte/resources/sessions.rb
+++ lib/dyte/resources/sessions.rb
@@ -15,12 +15,20 @@ module Dyte
       Session.new get_request("sessions/#{session_id}/participants").body.dig("data")
     end
 
+    def participant_details(session_id:, participant_id:)
+      Session.new get_request("sessions/#{session_id}/participants/#{participant_id}").body.dig("data")
+    end
+
     def update(meeting_id:, **attributes)
       Session.new put_request("sessions/#{meeting_id}", body: attributes).body.dig("data", "session")
     end
 
     def update_old(meeting_id:, body: {})
       Session.new put_request("sessions/#{meeting_id}", body: body).body.dig("data", "session")
+    end
+
+    def chat_messages(session_id:)
+      Session.new get_request("sessions/#{session_id}/chat").body.dig("data")
     end
   end
 end
