a87e92b9c313180cb3f76e546137cc09b0970b33
Nachiket Pusalkar <33430835+nachiket87@users.noreply.github.com>
2023-10-23 19:11:03 -0400

    chore: add organization fetch endpoint

--- lib/dyte/resources/organizations.rb
+++ lib/dyte/resources/organizations.rb
@@ -4,6 +4,10 @@ module Dyte
       Organization.new post_request("orgs", body: attributes).body.dig("data")
     end
 
+    def fetch(organization_id:)
+      Organization.new get_request("orgs/#{organization_id}").body.dig("data")
+    end
+
     def update(organization_id:, **attributes)
       Organization.new patch_request("orgs/#{organization_id}", body: attributes).body.dig("data")
     end
