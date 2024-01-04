3306f86686979887b22eacd13f783d66ab01b397
Nick Warwick <nicholaswarwick@icloud.com>
2024-01-04 09:15:11 -0800

    Add list support for Webhooks

--- test/dyte/resources/webhooks_test.rb
+++ test/dyte/resources/webhooks_test.rb
@@ -15,4 +15,15 @@ class WebhooksResourceTest < Minitest::Test
     assert_equal Dyte::Webhook, webhook.class
     assert_equal "blueberry", webhook.id
   end
+
+  def test_list
+    stub_request(:get, "#{Dyte::Client::BASE_URL}/webhooks")
+      .to_return(body: File.new("test/fixtures/webhooks/list.json"), headers: {content_type: "application/json"})
+
+    client = Dyte::Client.new(api_key: @api_key, organization_id: @organization_id)
+    webhooks = client.webhooks.list
+    assert_equal Dyte::Collection, webhooks.class
+    assert_equal Dyte::Webhook, webhooks.data.first.class
+    assert_equal "cherry", webhooks.data.first.id
+  end
 end
